7a7e69a888fdc65a1a73f1655f65e02a9d904382
SihyunSeo <tjtlgus4@naver.com>
2024-08-26 10:46:03 +0900

    blue-green

--- src/main/java/com/example/jenkins/TestController.java
+++ src/main/java/com/example/jenkins/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping(method = RequestMethod.GET, value="/test")
     public String test() {
-        return "v6";
+        return "v7";
     }
 }
